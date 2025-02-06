bfa4bbc480cb263ee6eab0cd40d054f8a6cc58fe
jeanport <pierreanatc522@gmail.com>
2025-02-06 16:25:28 -0300

    Create class UserType

---
+++ src/main/java/com/cliente/ws/rasmooplus/model/UserType.java
@@ -0,0 +1,21 @@
+package com.cliente.ws.rasmooplus.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "user_type")
+public class UserType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_type_id")
+    private Long id;
+    private String name;
+    private String description;
+}

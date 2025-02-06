2f433e4d93331c21810b6ce09533f9afce00b2fb
jeanport <pierreanatc522@gmail.com>
2025-02-06 15:29:22 -0300

    Iniciando o projeto

---
+++ src/main/java/com/cliente/ws/rasmooplus/WsRasmooPlussApplication.java
@@ -0,0 +1,13 @@
+package com.cliente.ws.rasmooplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRasmooPlussApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsRasmooPlussApplication.class, args);
+	}
+
+}

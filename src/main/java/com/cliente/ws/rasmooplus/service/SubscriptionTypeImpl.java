089dad989bcbca62de156e12f4c3ee1a341b6c4c
jeanport <pierreanatc522@gmail.com>
2025-02-06 21:21:38 -0300

    create class SubscriptionTypeServiceImpl

---
+++ src/main/java/com/cliente/ws/rasmooplus/service/SubscriptionTypeImpl.java
@@ -0,0 +1,47 @@
+package com.cliente.ws.rasmooplus.service;
+
+import com.cliente.ws.rasmooplus.model.SubscriptionType;
+import com.cliente.ws.rasmooplus.repository.SubscriptionTypeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SubscriptionTypeImpl implements ISubscriptionTypeService{
+
+    private final SubscriptionTypeRepository repository;
+
+
+    @Override
+    public List<SubscriptionType> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public SubscriptionType findById(Long id) {
+
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não encontrado"));
+    }
+
+    @Override
+    public SubscriptionType update(SubscriptionType subscriptionType) {
+        repository.findById(subscriptionType.getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não encontrado"));
+
+        return repository.save(subscriptionType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        var res = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não encontrado"));
+        repository.delete(res);
+    }
+
+    @Override
+    public Long create(SubscriptionType subscriptionType) {
+        return repository.save(subscriptionType).getId();
+    }
+}

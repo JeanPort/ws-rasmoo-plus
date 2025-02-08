39e748c51bd89279c0d159671be70f557df70c23
jeanport <pierreanatc522@gmail.com>
2025-02-08 11:00:35 -0300

    crate NotFoundException class

---
+++ src/main/java/com/cliente/ws/rasmooplus/exception/NotFoundExcption.java
@@ -0,0 +1,7 @@
+package com.cliente.ws.rasmooplus.exception;
+
+public class NotFoundExcption extends RuntimeException{
+    public NotFoundExcption(String message) {
+        super(message);
+    }
+}

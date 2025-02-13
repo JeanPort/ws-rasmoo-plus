905b04366d3410902140e68348d67206a4a6a476
jeanport <pierreanatc522@gmail.com>
2025-02-13 08:58:57 -0300

    create CustomerDto class

---
+++ src/main/java/com/cliente/ws/rasmooplus/dto/wsraspay/CustomerDto.java
@@ -0,0 +1,50 @@
+package com.cliente.ws.rasmooplus.dto.wsraspay;
+
+public class CustomerDto {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String cpf;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
